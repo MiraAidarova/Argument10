3174a6c7fa3db30cffe14c2ea9f9af9621f36e7b
task3 <miraajdarova2@gmail.com>
2022-10-07 17:21:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,81 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+     /*   System.out.println(method(1, 2, 3, 4, 5));
+    }
+    static int method(int... a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
+} */
+
+
+
+     /*   int[] sandar = {1, 2, 3, 4, 5, 6, 7};
+        for (int i = 0; i < sandar.length; i++) {
+            System.out.println(sandar[i] + "");
+        }
+        int sum = 0;
+        for (int a : sandar) {
+            sum += a;
+        }
+    }
+} */
+
+
+     /*   String[] at = {"Aigul", "Anara", "Rahat"};
+        for (int i = 0; i < at.length; i++) {
+            System.out.println(at[i]);
+        }
+        System.out.println();
+        for (String a : at) {
+            System.out.println("Men seni jakshi korom  " + a);
+        }
+    }
+} */
+
+     /**   String soz = "    Java   ";
+        String name = "Kanatbel";
+        String soz2 = "Java";
+        System.out.println(name.charAt(1));
+        String kesuu = name.substring(2);
+        System.out.println(kesuu);
+        System.out.println(name.substring(3));
+        System.out.println(name.contains("r"));
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(soz);
+        System.out.println(soz.trim());
+        System.out.println(name.equals(soz));
+        System.out.println("Barabar");
+        System.out.println(name.replace('a', 'u'));
+        System.out.println(name.length());
+        System.out.println(name.concat("Abdildaev"));
+
+    }
+}  */
+
+
+     /*   String bay = "tep ";
+        String ru = "Kitep";
+        System.out.println(ru.toUpperCase());
+        System.out.println(ru.trim());
+        System.out.println(ru.contains("a"));
+        System.out.println(ru.equals(bay));
+        System.out.println(ru.indexOf('t')); */
+    }
+}
+
+
+
+
+
+
+
